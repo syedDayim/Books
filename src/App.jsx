40a9fd37443e0f42d29121e6842d4069b2f15ed9
syedDayim <dayim1277@gmail.com>
2024-05-16 11:02:17 +0530

    Project & Tailwind Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import BookCreate from './components/BookCreate'
+
+function App() {
+
+  return (
+    <>
+      <BookCreate />
+    </>
+  )
+}
+
+export default App
